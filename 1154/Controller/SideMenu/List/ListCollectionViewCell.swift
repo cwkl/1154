031d6c8c6e793f6669450e4f4d14a3137886a65b
KWON JUNHYEOK <cwkl2540@gmail.com>
2019-02-07 19:20:05 +0900

    list pageview make

--- 1154/Controller/SideMenu/List/ListCollectionViewCell.swift
+++ 1154/Controller/SideMenu/List/ListCollectionViewCell.swift
@@ -8,6 +8,37 @@
 
 import UIKit
 
+protocol ListCollectionViewCellDelegate {
+    func tapCell(indexPath: IndexPath)
+}
+
 class ListCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellLabel: UILabel!
+    
+    var tapGesture = UITapGestureRecognizer()
+    var indexPath: IndexPath? {
+        didSet {
+            if indexPath?.item == 0 {
+                cellLabel.textColor = UIColor(red: 19/255, green: 69/255, blue: 99/255, alpha: 0.9) /* #134563 */
+            } else {
+                cellLabel.textColor = .lightGray
+            }
+        }
+    }
+    
+    var delegate: ListCollectionViewCellDelegate?
+    
+    override func awakeFromNib() {
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapCell))
+        tapGesture.isEnabled = true
+        cellLabel.isUserInteractionEnabled = true
+        cellLabel.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func tapCell() {
+        guard let indexPath = self.indexPath else { return }
+        
+        self.delegate?.tapCell(indexPath: indexPath)
+    }
     
 }
